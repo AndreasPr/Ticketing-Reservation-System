7a2467b0a4ab7906a66e602943e7878adde27fb8
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2023-05-03 18:42:24 -0400

    changes to signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from 'express';
-import {body} from 'express-validator';
+import {body, validationResult} from 'express-validator';
 const router = express.Router();
 
 router.post('/api/users/signup', [
@@ -11,10 +11,13 @@ router.post('/api/users/signup', [
             .isLength({min: 4, max: 20})
             .withMessage('Password must be between 4 and 20 characters')
     ], (req: Request, res: Response) => {
-    const {email, password} = req.body;
-    if (!email || typeof email !== 'string'){
-        res.status(400).send('Provide a valid email');
-    }
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).send(errors.array())
+        }
+
+        const {email, password} = req.body;
+        res.send({});
 });
 
 export {router as signupRouter};
