d0a2b6e9fd4ca337c12e5286f572710eb3b7079d
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2023-05-03 23:14:28 -0400

    addition of database and request validation errors and the middleware for errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 const router = express.Router();
 
 router.post('/api/users/signup', [
@@ -13,10 +16,12 @@ router.post('/api/users/signup', [
     ], (req: Request, res: Response) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()){
-            return res.status(400).send(errors.array())
+            throw new RequestValidationError(errors.array());
         }
+        
+        console.log('Creating a user...');
+        throw new DatabaseConnectionError();
 
-        const {email, password} = req.body;
         res.send({});
 });
 
