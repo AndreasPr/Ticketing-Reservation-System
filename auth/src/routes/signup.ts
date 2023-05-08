151777286c78ebc56719405bcdce246cadf39585
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2023-05-08 11:50:12 -0400

    addition of validateResult middleware for request validation in signin and signup services

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, {Request, Response} from 'express';
-import {body, validationResult} from 'express-validator';
+import {body} from 'express-validator';
 import jwt from 'jsonwebtoken';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateResult } from '../middlewares/validate-request';
 import {User} from '../models/user';
 
 const router = express.Router();
@@ -15,11 +15,9 @@ router.post('/api/users/signup', [
             .trim()
             .isLength({min: 4, max: 20})
             .withMessage('Password must be between 4 and 20 characters')
-    ], async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            throw new RequestValidationError(errors.array());
-        }
+    ], 
+    validateResult, 
+    async (req: Request, res: Response) => {
 
         const {email, password} = req.body;
         const existingUser = await User.findOne({email});
