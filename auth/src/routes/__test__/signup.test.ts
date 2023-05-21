cbcccb841a057f200f1f9dc5a736a611edfa10ec
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2023-05-20 20:27:55 -0400

    test invalid inputs in signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,30 @@ it('returns a 201 on successful signup', async () => {
     .expect(201);
 
 });
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'vgvccwer',
+        password: 'frgvfd'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an imvalid password', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: 'p'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with a missing email and password', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({})
+    .expect(400);
+});
